117a2e7576dc223c4feeb1a680e2d2821db44297
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2025-10-03 19:51:51 -0400

    fix: refactor index.html and add new parcel.js files

---
+++ dist/parcel/parcel.b24c69eb.js
@@ -0,0 +1,10 @@
+function $5002f5814ed49d1e$export$2e2bcd8739ae039(message) {
+    console.log(message);
+}
+$5002f5814ed49d1e$export$2e2bcd8739ae039('LIB');
+
+
+(0, $5002f5814ed49d1e$export$2e2bcd8739ae039)('PARCEL');
+
+
+//# sourceMappingURL=parcel.b24c69eb.js.map
